90d88296f1caa615f363e073dc0d6376648703c1
dvdmrs <dvdmrs09@gmail.com>
2024-03-05 23:34:10 +0000

    new file:   app/contact/contact.tsx 	new file:   app/law/law.tsx 	modified:   app/page.tsx 	new file:   app/privacy/privacy.tsx 	new file:   app/support/support.tsx 	new file:   app/x-scan/x-scan.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,16 @@
 import { Metadata } from "next"
 import { Button } from "components/Button/Button"
-import { LP_GRID_ITEMS } from "lp-items"
+
 
 export const metadata: Metadata = {
-  title: "Next.js Enterprise Boilerplate",
-  twitter: {
-    card: "summary_large_image",
-  },
+  title: "DMCS&T LLC",
   openGraph: {
-    url: "https://next-enterprise.vercel.app/",
+    url: "https://dmcomputerservices.net/",
     images: [
       {
         width: 1200,
         height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png",
+        url: "https://dmcomputerservices.net/logo.png",
       },
     ],
   },
@@ -26,18 +23,16 @@ export default function Web() {
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
-              Next.js Enterprise Boilerplate
+              DMCS&T LLC
             </h1>
             <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
-              Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
-              Experience rapid UI development, AI-powered code reviews, and an extensive suite of tools for a smooth and
-              enjoyable development process.
+              
             </p>
-            <Button href="https://github.com/Blazity/next-enterprise" className="mr-3">
+            <Button href="https://" className="mr-3">
               Get started
             </Button>
             <Button
-              href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
+              href="https://"
               intent="secondary"
             >
               Deploy Now
---
+++ app/privacy/privacy.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next"
+import { Button } from "components/Button/Button"
+
+
+export const metadata: Metadata = {
+  title: "DMCS&T LLC",
+  openGraph: {
+    url: "https://dmcomputerservices.net/privacy",
+    images: [
+      {
+        width: 1200,
+        height: 630,
+        url: "https://dmcomputerservices.net/logo.png",
+      },
+    ],
+  },
+}
